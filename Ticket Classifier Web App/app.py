77254444d29c50153025f2fcf7857d5ef809635b
Devavrath Tirukovela <devavrath.inc@gmail.com>
2023-10-07 00:37:47 +0530

    Update app.py

--- Ticket Classifier Web App/app.py
+++ Ticket Classifier Web App/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, jsonify
 import joblib
+import pickle
 
 app = Flask(__name__)
 
-# Load your trained model here
-# Example:
+# Load your model and vectorizer
 model = joblib.load('model/your_model.pkl')
+vectorizer = joblib.load('model/vectorizer.pkl')
 
 @app.route('/')
 def home():
@@ -16,12 +17,15 @@ def classify():
     if request.method == 'POST':
         ticket_text = request.form['ticket_text']
 
-        # Use your model to classify the ticket here
-        # Example:
-        prediction = model.predict([ticket_text])[0]
+        # Preprocess the input text using the loaded vectorizer
+        # You may need to adapt this preprocessing to match how the data was preprocessed during training
+        ticket_text = vectorizer.transform([ticket_text])  # Reshape to 2D array
+
+        # Make predictions using your model
+        prediction = model.predict(ticket_text)[0]
 
         # Return the prediction as JSON
-        return jsonify({'prediction': 'Sample Classification'})  # Replace with your actual prediction
+        return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
     app.run(debug=True)
