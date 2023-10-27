009394003b8dcc4f120421588a260e9b59eb439b
Devavrath Tirukovela <devavrath.inc@gmail.com>
2023-10-27 21:43:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 import joblib
 
 # Load your trained model
-with open('your_model.pkl', 'rb') as model_file:
-    model = joblib.load(model_file)
+model = joblib.load('your_model.pkl')
 vectorizer = joblib.load('vectorizer.pkl')
+
 # Create a Streamlit web app
 st.title('Ticket Classification App')
 st.write('Enter the ticket description and click the "Predict" button.')
@@ -14,10 +14,12 @@ ticket_description = st.text_input('Ticket Description')
 
 if st.button('Predict'):
     if ticket_description:
-        statement=vectorizer.transform(ticket_description)
+        # Preprocess the input using the vectorizer
+        statement = vectorizer.transform([ticket_description])
+
+        # Use your model to make predictions
         predicted_class = model.predict(statement)[0]
 
         st.write(f'Predicted Class: {predicted_class}')
     else:
         st.warning('Please enter a ticket description.')
-
